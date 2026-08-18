bdea54eda9acf0b05eeba98e87495266b3ab427c
Carlos Castillo <castillomartinezcarlos@gmail.com>
2026-08-18 20:42:14 +0200

    feat(ccp-008): build responsive portfolio header (#31)
    * chore: update settings.json
    
    * feat(ccp-007): build responsive portfolio header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { MainLayout } from './layout/MainLayout/MainLayout';
+import { MainLayout } from './layout/main-layout/main-layout';
 
 export const routes: Routes = [
   { path: 'home', component: MainLayout },
--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Header } from '../header/header';
 
 @Component({
   selector: 'ccp-main-layout',
-  imports: [],
-  templateUrl: './MainLayout.html',
+  imports: [Header],
+  templateUrl: './main-layout.html',
 })
 export class MainLayout {}
